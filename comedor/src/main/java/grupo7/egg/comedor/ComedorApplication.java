d2798667580f6124c9d1accc8a8ac38f935d05ca
Franco Scolari <franco.scolari@hotmail.com>
2021-11-24 21:58:55 -0300

    Agrego proyecto Spring

---
+++ comedor/src/main/java/grupo7/egg/comedor/ComedorApplication.java
@@ -0,0 +1,13 @@
+package grupo7.egg.comedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComedorApplication.class, args);
+	}
+
+}
---
+++ comedor/src/test/java/grupo7/egg/comedor/ComedorApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo7.egg.comedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
