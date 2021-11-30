d82695e9da529cee586414cfda13960d1d681c12
Franco Scolari <franco.scolari@hotmail.com>
2021-11-29 22:17:23 -0300

    Delete nutrividas directory

--- nutrividas/src/main/java/grupo7/egg/nutrividas/repositorios/TarjetaRepository.java
+++
@@ -1,8 +0,0 @@
-package grupo7.egg.nutrividas.repositorios;
-
-
-import grupo7.egg.nutrividas.entidades.Tarjeta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TarjetaRepository extends JpaRepository<Tarjeta,Long> {
-}
